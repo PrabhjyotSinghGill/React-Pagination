eb5b13367e0d384415a5311319af5cab526c0b01
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-01-25 21:37:29 -0500

    Pagination Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Posts from "./components/Posts.jsx";
-import axios from 'axios';
-import './App.css';
+import Pagination from "./components/Pagination.jsx";
+import axios from "axios";
+import "./App.css";
+import "../src/bootstrap.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -9,26 +11,30 @@ function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(15);
 
-  useEffect(() =>{
-    const fetchPosts = async () =>{
+  useEffect(() => {
+    const fetchPosts = async () => {
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
       setLoading(false);
-    }
+    };
     fetchPosts();
-  },[]);
+  }, []);
 
   //Get Current Posts
   const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage; 
-  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   console.log(posts);
   return (
     <div className="app mt-5">
-      <h1 className='text-primary mb-3'>Blog</h1>
+      <h1 className="text-primary mb-3">Blog</h1>
       <Posts posts={currentPosts} loading={loading}></Posts>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+      ></Pagination>
     </div>
   );
 }
